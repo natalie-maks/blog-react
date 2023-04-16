2c72e3d8308766ad3ffbff1f8098469b499a102c
natalie-maks <natasha.maxymenko@gmail.com>
2023-04-16 02:05:22 +0100

    Connected commentsForm component to graphql

--- components/CommentForm.js
+++ components/CommentForm.js
@@ -1,51 +1,112 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
+
+import { submitComment } from "@/services";
 
 const CommentForm = ({ slug }) => {
   const [error, setError] = useState(false);
   const [localStorage, setLocalStorage] = useState(null);
   const [successMessage, setSuccessMessage] = useState(false);
-  const commentEl = useRef();
-  const nameEl = useRef();
-  const emailEl = useRef();
-  const storeDataEl = useRef();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    comment: "",
+    storeData: false,
+  });
+
+  useEffect(() => {
+    setLocalStorage(window.localStorage);
+    const initalFormData = {
+      name: window.localStorage.getItem("name") ? window.localStorage.getItem("name") : "",
+      email: window.localStorage.getItem("email") ? window.localStorage.getItem("email") : "",
+      storeData:
+        window.localStorage.getItem("name") || window.localStorage.getItem("email") ? true : false,
+      comment: "",
+    };
+
+    console.log(initalFormData);
+
+    setFormData(initalFormData);
+  }, []);
+
+  const onInputChange = (e) => {
+    const { target } = e;
+    if (target.type === "checkbox") {
+      setFormData((prevState) => ({
+        ...prevState,
+        [target.name]: target.checked,
+      }));
+    } else {
+      setFormData((prevState) => ({
+        ...prevState,
+        [target.name]: target.value,
+      }));
+    }
+  };
 
   const handleCommentSubmit = () => {
     setError(false);
-
-    const { value: comment } = commentEl.current;
-    const { value: name } = nameEl.current;
-    const { value: email } = emailEl.current;
-    const { checked: storeData } = storeDataEl.current;
-
-    if (!comment || !name || !email) {
+    const { name, email, comment, storeData } = formData;
+    if (!name || !email || !comment) {
       setError(true);
       return;
     }
-
-    const commentObj = { name, email, comment, slug };
+    const commentObj = {
+      name,
+      email,
+      comment,
+      slug,
+    };
 
     if (storeData) {
       localStorage.setItem("name", name);
       localStorage.setItem("email", email);
     } else {
-      localStorage.removeItem("name", name);
-      localStorage.removeItem("email", email);
+      localStorage.removeItem("name");
+      localStorage.removeItem("email");
     }
+
+    submitComment(commentObj).then((res) => {
+      setSuccessMessage(true);
+      setTimeout(() => {
+        setSuccessMessage(false);
+      }, 3000);
+    });
   };
 
   return (
     <div>
       CommentForm
-      <form onSubmit={handleCommentSubmit}>
-        <textarea ref={commentEl} placeholder="Comment" name="comment" />
-        <input ref={nameEl} type="text" placeholder="Name" name="name" />
-        <input ref={emailEl} type="email" placeholder="Email" name="email" />
-        {error && <p>All fields are requiered</p>}
-        <label htmlFor="storeData">Save email and name</label>
-        <input fer={storeDataEl} type="checkbox" id="storeData" name="storeData" />
-        <button>Submit</button>
-        {successMessage && <span>Comment submitted for review</span>}
-      </form>
+      <textarea
+        value={formData.comment}
+        onChange={onInputChange}
+        placeholder="Comment"
+        name="comment"
+      />
+      <input
+        value={formData.name}
+        onChange={onInputChange}
+        type="text"
+        placeholder="Name"
+        name="name"
+      />
+      <input
+        value={formData.email}
+        onChange={onInputChange}
+        type="email"
+        placeholder="Email"
+        name="email"
+      />
+      {error && <p>All fields are requiered</p>}
+      <label htmlFor="storeData">Save email and name</label>
+      <input
+        value={formData.storeData}
+        onChange={onInputChange}
+        type="checkbox"
+        id="storeData"
+        name="storeData"
+      />
+      <button onClick={handleCommentSubmit}>Submit</button>
+      {successMessage && <span>Comment submitted for review</span>}
     </div>
   );
 };
