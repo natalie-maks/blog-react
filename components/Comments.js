926156353e594f4e438cbd9dc3d804007a299721
natalie-maks <natasha.maxymenko@gmail.com>
2023-04-19 00:29:51 +0100

    Styled Comments component

--- components/Comments.js
+++ components/Comments.js
@@ -1,16 +1,20 @@
 import React from "react";
+import moment from "moment";
 
 const Comments = ({ comments }) => {
   return (
-    <div>
-      Comments
+    <section className="my-8">
+      <h2 className="text-2xl font-semibold mb-6">Comments ({comments.length})</h2>
       {comments.map((comment) => (
-        <div key={comment.createdAt}>
-          {comment.name}
-          {comment.comment}
+        <div key={comment.createdAt} className="my-4">
+          <address className="flex justify-between">
+            <span className="font-semibold mb-1 not-italic">{comment.name}</span>
+            <time className="opacity-60">{moment(comment.createdAt).format("MMM DD, YYYY")}</time>
+          </address>
+          <p className="text-lg">{comment.comment}</p>
         </div>
       ))}
-    </div>
+    </section>
   );
 };
 
